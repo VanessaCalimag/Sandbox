98aab50297c6daf476deed0c3c0bc8bd4c23b4b4
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2019-03-12 15:34:39 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,12 @@
 """Vanessa Calimag"""
+
+
+def main():
+    password = input("Enter a password: ")
+    if len(password) < 10:
+        print("Minimum number of characters is 10 - Enter a password.")
+    else:
+        print('*').format(password)
+
+
+main()
